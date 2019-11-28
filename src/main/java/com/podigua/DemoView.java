695853a73890e17449740e5de57b38cd38ad5a4b
jiabiaoli <jiabiaoli@boco.com.cn>
2019-11-28 09:57:43 +0800

    init

--- src/main/java/com/podigua/DemoView.java
+++ src/main/java/com/podigua/DemoView.java
@@ -3,6 +3,6 @@ package com.podigua;
 import de.felixroske.jfxsupport.AbstractFxmlView;
 import de.felixroske.jfxsupport.FXMLView;
 
-@FXMLView(value = "/views/demo.fxml")
+@FXMLView(value = "/view/demo.fxml")
 public class DemoView extends AbstractFxmlView {
 }
--- src/main/java/com/podigua/OpenView.java
+++ src/main/java/com/podigua/OpenView.java
@@ -3,6 +3,6 @@ package com.podigua;
 import de.felixroske.jfxsupport.AbstractFxmlView;
 import de.felixroske.jfxsupport.FXMLView;
 
-@FXMLView(value = "/views/open.fxml",title = "open")
+@FXMLView(value = "/view/open.fxml",title = "open")
 public class OpenView extends AbstractFxmlView {
 }
